0ee2bb10cb67609d9ab2bc2e8d2cc39dbf61d7d2
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-06-28 18:06:57 -0300

    fix: headers accept axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
 });
