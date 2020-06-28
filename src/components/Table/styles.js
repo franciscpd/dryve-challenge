662f928198728566dbdc681f1fbab31da387e7a2
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-06-28 16:03:56 -0300

    fix: head style

--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -4,5 +4,12 @@ export const Table = styled.div`
   & > div {
     border-radius: 4px;
     overflow: hidden;
+
+    .MuiTableCell-head {
+      font-size: 10px;
+      font-weight: 500;
+      text-transform: uppercase;
+      color: rgba(0, 0, 0, 0.47);
+    }
   }
 `;
