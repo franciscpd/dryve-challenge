05c3007b50f6e08634bf5b621df7e928a5b23ba0
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-06-28 12:22:59 -0300

    feat: add component table

---
+++ src/components/Table/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MaterialTable from 'material-table';
+
+import * as S from './styles';
+
+const defaultOptions = {
+  search: false,
+  paging: false,
+  showTitle: false,
+  toolbar: false,
+};
+
+const Table = ({ options, data, columns, ...rest }) => (
+  <S.Table>
+    <MaterialTable
+      options={{ ...defaultOptions, ...options }}
+      columns={columns.slice(0).map((it) => ({ ...it }))}
+      data={Array.isArray(data) ? data.slice(0).map((it) => ({ ...it })) : data}
+      {...rest}
+    />
+  </S.Table>
+);
+
+Table.propTypes = {
+  options: PropTypes.shape({}),
+  data: PropTypes.arrayOf(PropTypes.shape({})),
+  columns: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+Table.defaultProps = {
+  options: {},
+  data: [],
+  columns: [],
+};
+
+export default Table;
---
+++ src/components/Table/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Table = styled.div`
+  & > div {
+    border-radius: 4px;
+    overflow: hidden;
+  }
+`;
