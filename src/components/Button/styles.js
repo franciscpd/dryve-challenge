a38eafdfd012e9d479faed47f30fb87be8b068c4
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-06-29 21:07:18 -0300

    fix: button component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,14 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import MuiButton from '@material-ui/core/Button';
 
 export const Button = styled(MuiButton)`
-  background: #6200ee;
-  color: #fff;
+  ${(props) => {
+    if (props.variant === 'contained') {
+      return css`
+        background: #6200ee;
+        color: #fff;
+
+        &:hover {
+          background: #6200ee;
+        }
+      `;
+    }
+
+    return css`
+      color: #6200ee;
+    `;
+  }}
+
   font-size: 14px;
   padding: 5px 13px;
   font-weight: 500;
-
-  &:hover {
-    background: #6200ee;
-  }
 `;
